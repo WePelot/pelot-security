5048ca5d55216511c3650061d7c276ef6a127e70
WePelot <hcj1581@163.com>
2017-10-25 23:08:36 +0800

    [add]pelot-security项目搭建

---
+++ pelot-security-demo/src/main/java/com/pelot/security/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.pelot.security.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by hongcj
+ * 2017/10/25 22:57.
+ */
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello spring security";
+    }
+}
